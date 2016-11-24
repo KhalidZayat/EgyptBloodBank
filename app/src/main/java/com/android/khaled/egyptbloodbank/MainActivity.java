9df4b0729b0c3e0134ec9b822867707ad06a5ad4
KhalidZayat <khalid.mahmoud.mohamed@gmail.com>
2016-11-24 17:56:56 +0200

    blood changed

--- app/src/main/java/com/android/khaled/egyptbloodbank/MainActivity.java
+++ app/src/main/java/com/android/khaled/egyptbloodbank/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,6 +21,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,AdapterView.OnItemSelectedListener {
+
+    String Blood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,8 +72,7 @@ public class MainActivity extends AppCompatActivity
         // On selecting a spinner item
         String item = parent.getItemAtPosition(position).toString();
 
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
@@ -132,4 +134,58 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    @FromXML
+    public void onClick(View v)
+    {
+        TextView Main_A_Plus = (TextView)findViewById(R.id.Main_A_Plus);
+        TextView Main_A_minus = (TextView)findViewById(R.id.Main_A_minus);
+        TextView Main_B_Plus = (TextView)findViewById(R.id.Main_B_Plus);
+        TextView Main_B_Minus = (TextView)findViewById(R.id.Main_B_Minus);
+        TextView Main_O_Plus = (TextView)findViewById(R.id.Main_O_Plus);
+        TextView Main_O_Minus = (TextView)findViewById(R.id.Main_O_Minus);
+        TextView Main_AB_Plus = (TextView)findViewById(R.id.Main_AB_Plus);
+        TextView Main_AB_Minus = (TextView)findViewById(R.id.Main_AB_Minus);
+        TextView Main_Any = (TextView)findViewById(R.id.Main_Any);
+
+        Main_A_Plus.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        Main_A_Plus.setBackgroundDrawable(getResources().getDrawable(R.drawable.border));
+
+        Main_A_minus.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        Main_A_minus.setBackgroundDrawable(getResources().getDrawable(R.drawable.border));
+
+        Main_B_Plus.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        Main_B_Plus.setBackgroundDrawable(getResources().getDrawable(R.drawable.border));
+
+        Main_B_Minus.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        Main_B_Minus.setBackgroundDrawable(getResources().getDrawable(R.drawable.border));
+
+        Main_O_Plus.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        Main_O_Plus.setBackgroundDrawable(getResources().getDrawable(R.drawable.border));
+
+        Main_O_Minus.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        Main_O_Minus.setBackgroundDrawable(getResources().getDrawable(R.drawable.border));
+
+        Main_AB_Plus.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        Main_AB_Plus.setBackgroundDrawable(getResources().getDrawable(R.drawable.border));
+
+        Main_AB_Minus.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        Main_AB_Minus.setBackgroundDrawable(getResources().getDrawable(R.drawable.border));
+
+        Main_Any.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        Main_Any.setBackgroundDrawable(getResources().getDrawable(R.drawable.border));
+
+        TextView textView = (TextView)v;
+        textView.setTextColor(getResources().getColor(R.color.White));
+        textView.setBackgroundDrawable(getResources().getDrawable(R.drawable.red_border));
+
+        Blood = textView.getText().toString();
+        // Showing selected blood
+        Toast.makeText(getBaseContext(), "Selected: " + Blood, Toast.LENGTH_SHORT).show();
+
+
+    }
 }
+
+
